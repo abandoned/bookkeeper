900320bc527fd82790691357c81e7e0e634d9849
snl <snl@theorydot.com>
2009-11-03 10:16:44 +0000

    Added ar-extensions to gem manifest

--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem 'ar-extensions',
+             :source => 'http://gemcutter.org',
+             :version => '>= 0.9.2'
   config.gem 'authlogic',
              :source => 'http://gemcutter.org',
              :version => '>= 2.1.2'

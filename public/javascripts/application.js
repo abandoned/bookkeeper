e1ebaaeab2820875deba1e41fd4a8d15d2000864
snl <snl@theorydot.com>
2009-11-17 11:30:31 +0000

    autocomplete css with some other stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-  setTimeout("$('#flash_notice').slideUp('fast')",3000);
+  setTimeout("$('#flash_notice').slideUp('fast')", 3000);
   $('select.autocomplete').select_autocomplete();
 })
+
+(function($) {
+		  
+	$.fn.use_label_as_default = function() {
+	  console.log("ok");
+  };
+})(jQuery);

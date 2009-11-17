1585bb761ded7402f3846f907117874c93d17efa
Rob Holland <rob@the-it-refinery.co.uk>
2009-11-17 10:35:38 +0000

    Make account an autocompleting select.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   setTimeout("$('#flash_notice').slideUp('fast')",3000);
-  $(select.autocomplete).select_autocomplete();
+  $('select.autocomplete').select_autocomplete();
 })

47469c65d76c54707c975dc07bb036918494bdf4
snl <snl@theorydot.com>
2009-12-23 23:22:06 +0000

    Trying to slim_attributes and slim_scrooge

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+require 'slim_attributes'
+
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
@@ -34,7 +36,9 @@ Rails::Initializer.run do |config|
              :version => '>= 1.4.1'
   config.gem 'will_paginate',
              :version => '~> 2.3.11'
-
+  config.gem 'slim_scrooge',
+             :version => '>= 1.0.3'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]

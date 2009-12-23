4e08a9cb759b2031364558b3b788d56228626c20
snl <snl@theorydot.com>
2009-12-23 23:34:40 +0000

    revert

--- config/environment.rb
+++ config/environment.rb
@@ -34,8 +34,6 @@ Rails::Initializer.run do |config|
              :version => '>= 1.4.1'
   config.gem 'will_paginate',
              :version => '~> 2.3.11'
-  config.gem 'slim_scrooge',
-             :version => '>= 1.0.3'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

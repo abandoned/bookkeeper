08e9d27203aea02fa6b72ad8efc6b22f32f2a91c
snl <snl@theorydot.com>
2010-01-07 20:23:41 +0000

    css melee

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
              :lib => "validatable"
   config.gem 'nokogiri'
   config.gem 'responders'
-  config.gem 'will_paginate'
+  config.gem 'will_paginate'  
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

7e24398b4058ac9c9eb932943873913ef537c9fa
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-04-17 02:06:24 +0100

    Added rescue to perform method in the import model

--- app/models/import.rb
+++ app/models/import.rb
@@ -43,56 +43,61 @@ class Import < ActiveRecord::Base
   end
   
   def perform
-    first_line = true
-    @ledger_items = []
+    begin
+      first_line = true
+      @ledger_items = []
     
-    FasterCSV.parse(@parsable) do |row|
+      FasterCSV.parse(@parsable) do |row|
       
-      # Skip first if title col
-      if first_line
-        first_line = false
-        next if mapping.has_title_row?
+        # Skip first if title col
+        if first_line
+          first_line = false
+          next if mapping.has_title_row?
+        end
+      
+        # Sign
+        sign = mapping.reverses_sign? ? -1 : 1
+      
+        t = LedgerItem.new(:account => account)
+      
+        # Format date
+        if mapping.day_follows_month?
+          t.transacted_on = row[mapping.date_row - 1]
+        else
+          t.transacted_on = Date.strptime(row[mapping.date_row - 1], '%d/%m/%Y')
+        end
+      
+        # Money entries
+        t.total_amount = row[mapping.total_amount_row - 1].gsub(/[^0-9.-]/, '').to_f * sign
+        t.tax_amount = row[mapping.tax_amount_row - 1].gsub(/[^0-9.-]/, '').to_f * sign unless mapping.tax_amount_row.blank?
+        t.currency = mapping.currency
+      
+        # Descriptive entries
+        unless mapping.description_row.blank?
+          t.description = row[mapping.description_row - 1]
+        end
+        unless mapping.second_description_row.blank? || row[mapping.second_description_row - 1].blank?
+          t.description << " " + row[mapping.second_description_row - 1]
+        end
+      
+        @ledger_items << t if t.valid?
       end
-      
-      # Sign
-      sign = mapping.reverses_sign? ? -1 : 1
-      
-      t = LedgerItem.new(:account => account)
-      
-      # Format date
-      if mapping.day_follows_month?
-        t.transacted_on = row[mapping.date_row - 1]
+    
+      # Calculate ending balance
+      ending_balance_confirmation = @account.ledger_items.sum("total_amount").to_f.round(2) + @ledger_items.sum { |t| t.total_amount }.to_f.round(2)
+    
+      if (ending_balance.to_f - ending_balance_confirmation.to_f).abs  < 0.01
+        ActiveRecord::Base.transaction do
+          @ledger_items.each { |i| i.save! }
+        end
+        self.message = "#{transaction_count} transactions imported"
+        succeed!
       else
-        t.transacted_on = Date.strptime(row[mapping.date_row - 1], '%d/%m/%Y')
+        self.message = "Ending balance of #{ending_balance_confirmation} did not match expected balance of #{ending_balance}"
+        fail!
       end
-      
-      # Money entries
-      t.total_amount = row[mapping.total_amount_row - 1].gsub(/[^0-9.-]/, '').to_f * sign
-      t.tax_amount = row[mapping.tax_amount_row - 1].gsub(/[^0-9.-]/, '').to_f * sign unless mapping.tax_amount_row.blank?
-      t.currency = mapping.currency
-      
-      # Descriptive entries
-      unless mapping.description_row.blank?
-        t.description = row[mapping.description_row - 1]
-      end
-      unless mapping.second_description_row.blank? || row[mapping.second_description_row - 1].blank?
-        t.description << " " + row[mapping.second_description_row - 1]
-      end
-      
-      @ledger_items << t if t.valid?
-    end
-    
-    # Calculate ending balance
-    ending_balance_confirmation = @account.ledger_items.sum("total_amount").to_f.round(2) + @ledger_items.sum { |t| t.total_amount }.to_f.round(2)
-    
-    if (ending_balance.to_f - ending_balance_confirmation.to_f).abs  < 0.01
-      ActiveRecord::Base.transaction do
-        @ledger_items.each { |i| i.save! }
-      end
-      self.message = "#{transaction_count} transactions imported"
-      succeed!
-    else
-      self.message = "Ending balance of #{ending_balance_confirmation} did not match expected balance of #{ending_balance}"
+    rescue
+      self.message = "???"
       fail!
     end
   end

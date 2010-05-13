8a2713c12291dd97819a5b4c3211ea2ee4c49b6f
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-05-13 21:52:40 +0100

    Upgraded compass.

--- config/compass.rb
+++ config/compass.rb
@@ -1,3 +1,4 @@
+# This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
 project_path = RAILS_ROOT if defined?(RAILS_ROOT)
@@ -5,10 +6,6 @@ project_path = RAILS_ROOT if defined?(RAILS_ROOT)
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
-images_dir = "public/images"
-javascripts_dir = "public/javascripts"
+environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-http_images_path = "/images"
-http_stylesheets_path = "/stylesheets"
-http_javascripts_path = "/javascripts"

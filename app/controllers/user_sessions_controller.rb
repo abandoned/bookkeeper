efbe56adc5b2dad746b6fb22a2104d061dcfeae7
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-05-25 15:04:44 +0100

    Login now works with Web-app-theme

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,21 +1,24 @@
 class UserSessionsController < InheritedResources::Base
+  layout "sign"
+
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user,    :only => [:destroy]
-  
+
   actions :new, :create, :destroy
-  
+
   def create
     create! do |success, failure|
       success.html { redirect_back_or_default ledger_items_path }
       failure.html {
-        flash.now[:failure] = 'Username or password is incorrect.'
+        flash.now[:error] = 'Username or password is incorrect.'
         render :action => :new
       }
     end
   end
-  
+
   def destroy
     current_user_session.destroy
-    redirect_to root_path
+    flash[:notice] = "You are logged out."
+    redirect_to new_user_session_path
   end
 end

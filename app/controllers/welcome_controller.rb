678642cacc51280652cbf733e669ca4a85e68659
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-05-25 15:02:26 +0100

    Welcome controller only redirects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to ledger_items_path if current_user
+    redirect_to current_user ?
+      ledger_items_path : new_user_session_path
   end
 end

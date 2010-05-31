7cf8ef8393b244274d9ca9b345b2323db7367495
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-05-31 19:55:30 +0100

    Remember last query on transactions page

--- app/controllers/ledger_items_controller.rb
+++ app/controllers/ledger_items_controller.rb
@@ -1,24 +1,20 @@
 class LedgerItemsController < InheritedResources::Base
   belongs_to    :account, :optional => true
-  
   respond_to    :html
   respond_to    :csv, :only => [:index]
   defaults      :resource_class         => LedgerItem,
                 :collection_name        => 'ledger_items',
                 :instance_name          => 'ledger_item',
                 :route_collection_name  => 'transactions', :route_instance_name => 'transaction'
-  
   before_filter :require_user
   before_filter :find_cart,
                 :only => [:index, :add_to_cart, :balance_cart, :save_cart]
-  
   def index
     if params[:query]
       session[:query] = params[:query]
     elsif session[:query]
       params[:query] = session[:query]
     end
-
     # TODO Refactor below.
     if request.format.csv?
       unless params[:account].blank?
@@ -26,10 +22,8 @@ class LedgerItemsController < InheritedResources::Base
       else
         filename = 'ledger'
       end
-      
       headings = ['transacted on', 'account', 'currency', 'total amount', 'tax amount', 'description', 'sender', 'recipient', 'match']
       output = FasterCSV.generate_line(headings)
-      
       data = end_of_association_chain.
         scope_by(params[:query]).
         all(
@@ -58,26 +52,20 @@ class LedgerItemsController < InheritedResources::Base
         ]
         output << FasterCSV.generate_line(data)
       end
-      
       return send_data(output, :filename => "#{filename}.#{Time.now.strftime('%y%m%d%H%M%S')}.csv")
     end
-    
     calculate_totals
     index!
   end
-  
   def edit
     @ledger_item = LedgerItem.find params[:id]
     @ledger_items = [@ledger_item]
   end
-  
   def add_to_cart
     ledger_item = LedgerItem.find(params[:id])
     @cart.add(ledger_item)
-    
     redirect_to collection_path
   end
-  
   def balance_cart
     account = Account.find(params[:ledger_item][:account_id])
     match = Match.new(:ledger_items => @cart.ledger_items)
@@ -88,10 +76,8 @@ class LedgerItemsController < InheritedResources::Base
     else
       flash[:notice] = 'Reconciliation failed'
     end
-    
     redirect_to collection_path
   end
-  
   def save_cart
     if Match.create(:ledger_items => @cart.ledger_items)
       flash[:notice] = 'Transactions successfully reconciled'
@@ -99,16 +85,12 @@ class LedgerItemsController < InheritedResources::Base
     else
       flash.now[:failure] = 'Reconciliation failed'
     end
-    
     redirect_to collection_path
   end
-  
   def empty_cart
     reset_cart
-    
     redirect_to collection_path
   end
-  
   def multiple
     if request.get?
       @ledger_item = LedgerItem.new
@@ -125,16 +107,13 @@ class LedgerItemsController < InheritedResources::Base
           end
         end
         flash[:notice] = 'Transactions successfully saved.'
-
         redirect_to collection_path
       rescue Exception => e
         flash[:failure] = 'Transactions failed to save.'
       end
     end
   end
-  
   private
-  
   def collection
     @ledger_items ||= end_of_association_chain.
       scope_by(params[:query]).
@@ -143,15 +122,12 @@ class LedgerItemsController < InheritedResources::Base
       :include => [:sender, :recipient, :account],
       :order => 'ledger_items.transacted_on ASC')
   end
-  
   def find_cart
     @cart ||= session[:cart] ||= Cart.new
   end
-  
   def reset_cart
     @cart = session[:cart] = Cart.new
   end
-  
   # Iterates over ledger items in the collection, summing up the totals
   # for each currency
   def calculate_totals

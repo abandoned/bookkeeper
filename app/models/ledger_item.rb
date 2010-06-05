b4a0c41d5fd9b0dbcd6ec3d4eac8a362c19003e8
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-06-05 10:04:15 -0400

    Added perspective scope to ledger item

--- app/models/ledger_item.rb
+++ app/models/ledger_item.rb
@@ -32,7 +32,7 @@ class LedgerItem < ActiveRecord::Base
   belongs_to :match
   belongs_to :sender,     :class_name => 'Contact'
   belongs_to :recipient,  :class_name => 'Contact'
-  
+
   validates_associated      :sender, :recipient, :account
   validates_presence_of     :account, :currency, :transacted_on
   validates_numericality_of :total_amount
@@ -41,13 +41,13 @@ class LedgerItem < ActiveRecord::Base
   validate                  :currency_must_validate
   validate                  :tax_amount_must_validate
   validate                  :perspective_must_validate
-  
+
   before_validation :default_blank_tax_amount_to_0
   before_create :set_transacted_on_to_today
   after_create  :create_matches
   after_update  :update_matches
   after_destroy :delete_matches
-  
+
   named_scope :matched,   :conditions => 'match_id IS NOT NULL'
   named_scope :unmatched, :conditions => 'match_id IS NULL'
   named_scope :debit,     :conditions => 'total_amount > 0'
@@ -60,32 +60,45 @@ class LedgerItem < ActiveRecord::Base
     end
   }
   named_scope :from_date, lambda { |date|
-    { :conditions => ['transacted_on >= ?', date] }
+    unless date.nil?
+      { :conditions => ['transacted_on >= ?', date] }
+    end
   }
   named_scope :to_date, lambda { |date|
-    { :conditions => ['transacted_on <= ?', date] }
+    unless date.nil?
+      { :conditions => ['transacted_on <= ?', date] }
+    end
   }
-  
+
+  named_scope :perspective, lambda { |id|
+    unless id.nil?
+      { :joins => 'INNER JOIN contacts AS senders ON senders.id = sender_id
+                   INNER JOIN contacts AS recipients ON recipients.id = recipient_id',
+        :conditions => ['(senders.id = ? AND ledger_items.total_amount < 0) OR (recipients.id = ? AND ledger_items.total_amount > 0)', id, id] }
+    end
+  }
+
   def self.scope_by(query)
     scope = scoped({})
     return scope if query.blank?
     query.split(';').each do |q|
       case q.strip.upcase
-        
+
       # Scope by total amount
       when /^(=|<|<=|>|>=|<>)\s*-?([0-9.,]+)$/
         scope = scope.scoped({
           :conditions => ["ABS(total_amount) #{$1} ?", $2.gsub(/,/, '').to_f]
         })
-      
+
       # Scope by contact name
       when /^BY\s+(.*)$/
         scope = scope.scoped({
           :joins => 'INNER JOIN contacts AS senders ON senders.id = sender_id
                      INNER JOIN contacts AS recipients ON recipients.id = recipient_id',
-          :conditions => ['UPPER(senders.name) = ? OR UPPER(recipients.name) = ?', $1, $1] 
+          :conditions => ['UPPER(senders.name) = ? OR UPPER(recipients.name) = ?', $1, $1]
+
         })
-      
+
       # Scope by account name
       when /^IN\s+(.*)$/
         scope = scope.scoped({
@@ -100,7 +113,7 @@ class LedgerItem < ActiveRecord::Base
             :conditions => ['transacted_on = ?', date.to_date]
           })
         end
-        
+
       # Scope by start date
       when /^SINCE\s+(.*)$/
         date = Chronic.parse($1)
@@ -109,7 +122,7 @@ class LedgerItem < ActiveRecord::Base
             :conditions => ['transacted_on >= ?', date.to_date]
           })
         end
-      
+
       # Scope by end date
       when /^UNTIL\s+(.*)$/
         date = Chronic.parse($1)
@@ -118,18 +131,18 @@ class LedgerItem < ActiveRecord::Base
             :conditions => ['transacted_on <= ?', date.to_date]
           })
         end
-      
+
       # Scope by match status
       when /^\s*NOT MATCHED\s*$/
         scope = scope.scoped({
           :conditions => 'match_id IS NULL'
         })
-      
+
       when /^\s*MATCHED\s*$/
         scope = scope.scoped({
           :conditions => 'match_id IS NOT NULL'
         })
-      
+
       # Scope by description
       when /\w/
         if RAILS_ENV == "production"
@@ -143,42 +156,42 @@ class LedgerItem < ActiveRecord::Base
         end
       end
     end
-    
+
     scope
-  end  
-  
+  end
+
   def account_name
     self.account.name if self.account
   end
-  
+
   def sender_name
     self.sender.name if self.sender
   end
-  
+
   def recipient_name
     self.recipient.name if self.recipient
   end
-  
+
   def matched?
     !self.match_id.nil?
   end
-  
+
   def matchable?
     self.sender && self.recipient
   end
-  
+
   def debit?
     self.total_amount > 0
   end
-  
+
   def credit?
     self.total_amount < 0
   end
-  
+
   def matches
     self.match.ledger_items.reject { |i| i.id == self.id }
   end
-  
+
   # TODO Refactor these out of here.
   CURRENCY_SYMBOLS = {
     'USD' => '$',
@@ -187,50 +200,52 @@ class LedgerItem < ActiveRecord::Base
     'CAD' => 'C$',
     'JPY' => '¥'
   }
-  
+
   def currency_symbol
     CURRENCY_SYMBOLS[self.currency]
   end
-  
+
   private
-  
+
   def set_transacted_on_to_today
     self.transacted_on = Date.today if self.transacted_on.nil?
   end
-  
+
   def currency_must_validate
     unless ISO4217::CODE.has_key?(self.currency)
       errors.add(:currency, 'is invalid')
     end
   end
-  
+
   def tax_amount_must_validate
     tax_amount_must_not_exceed_total_amount
     tax_amount_must_have_same_sign_as_total_amount
   end
-  
+
   def tax_amount_must_not_exceed_total_amount
     if self.tax_amount.abs - self.total_amount.abs > 0
       errors.add(:tax_amount, 'may not exceed total amount')
     end
   end
-  
+
   def tax_amount_must_have_same_sign_as_total_amount
     if self.tax_amount * self.total_amount < 0
       errors.add(:tax_amount, 'must have same sign as total amount')
     end
   end
-  
+
   # If self is part of the transaction, i.e. is sender and/or recipient,
-  # she should not be solely on the receiving end if transacted_amount is 
+  # she should not be solely on the receiving end if transacted_amount is
+
   # negative and similarly, should not be solely on the sending end if the
-  # transacted amount is positive. Either case would imply we are recording 
+  # transacted amount is positive. Either case would imply we are recording
+
   # in the ledger of the other party, not that of the self.
   def perspective_must_validate
     self_must_not_send_debit
     self_must_not_receive_credit
   end
-    
+
   def self_must_not_receive_credit
     if self.credit? &&
        self.recipient &&
@@ -240,17 +255,20 @@ class LedgerItem < ActiveRecord::Base
       errors.add_to_base('Not set up from perspective of self')
     end
   end
-  
+
   def self_must_not_send_debit
     if self.debit? &&
-       self.sender && 
-       self.sender.self? && 
-       self.recipient && 
+       self.sender &&
+
+       self.sender.self? &&
+
+       self.recipient &&
+
        !self.recipient.self?
       errors.add_to_base('Not set up from perspective of self')
     end
   end
-  
+
   def create_matches
     unless self.matched?
       self.account.rules.each do |rule|
@@ -258,15 +276,16 @@ class LedgerItem < ActiveRecord::Base
       end
     end
   end
-  
+
   def update_matches
     if self.matched? && self.changed? && !self.match_id_changed?
       matches = self.matches
       if matches.size == 1
-        
+
         # Update match
         i = matches.first
-        if (i.total_amount + self.total_amount).to_f.abs > 0.01 || 
+        if (i.total_amount + self.total_amount).to_f.abs > 0.01 ||
+
             i.sender_id != self.recipient_id ||
             i.recipient_id != self.sender_id
           i.update_attributes(
@@ -276,7 +295,7 @@ class LedgerItem < ActiveRecord::Base
           )
         end
       elsif matches.size > 1
-        
+
         # Destroy matches
         self.matches.each { |i| i.update_attribute(:match_id, nil) }
         self.match.destroy
@@ -284,13 +303,13 @@ class LedgerItem < ActiveRecord::Base
       end
     end
   end
-  
+
   def delete_matches
     if self.matched?
       match.destroy
     end
   end
-  
+
   def default_blank_tax_amount_to_0
     if tax_amount.blank?
       self.tax_amount = 0
--- spec/models/ledger_item_spec.rb
+++ spec/models/ledger_item_spec.rb
@@ -12,38 +12,38 @@ describe LedgerItem do
                            :account => @account,
                            :transacted_on => Date.new(2009,1,31))
   end
-  
+
   it "should belong to a account" do
     @ledger_item.account = nil
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should not have a tax amount that exceeds the total amount when transaction is a debit" do
     @ledger_item.tax_amount = 21
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should not have a tax amount that exceeds the total amount when transaction is a credit" do
     @ledger_item.total_amount = -20
     @ledger_item.tax_amount = -21
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should not have a tax amount that is the inverse sign of the total amount" do
     @ledger_item.tax_amount = -1
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should default tax_amount to 0 if tax_amount is nil" do
     @ledger_item.tax_amount = nil
     @ledger_item.should be_valid
   end
-  
+
   it "should have a valid currency code" do
     @ledger_item.currency = "ZZZ"
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should validate if transaction is between selves" do
     @sender.self = true
     @sender.save
@@ -51,47 +51,70 @@ describe LedgerItem do
     @recipient.save
     lambda { @ledger_item.save! }.should_not raise_error
   end
-  
+
   it "should validate if transacting parties are not defined" do
     @ledger_item.sender = nil
     @ledger_item.recipient = nil
     lambda { @ledger_item.save! }.should_not raise_error
   end
-  
+
   it "should not validate if debit and not from perspective of self" do
     @sender.self = true
     @sender.save
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should not validate if credit and not from perspective of self" do
     @recipient.self = true
     @recipient.save
     @ledger_item.total_amount = -20.0
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
   it "should not total 0" do
     @ledger_item.total_amount = 0
     lambda { @ledger_item.save! }.should raise_error(ActiveRecord::RecordInvalid)
   end
-  
+
+  describe "scopes" do
+    it "should scope by contact" do
+      LedgerItem.contact(@sender).count.should eql 1
+      LedgerItem.contact(@recipient).count.should eql 1
+      LedgerItem.contact(Factory(:contact)).count.should eql 0
+    end
+
+    it "should scope by from date" do
+      LedgerItem.from_date('2009-01-01').count.should eql 1
+      LedgerItem.from_date('2010-01-01').count.should eql 0
+    end
+
+    it "should scope by to date" do
+      LedgerItem.to_date('2009-01-01').count.should eql 0
+      LedgerItem.to_date('2010-01-01').count.should eql 1
+    end
+
+    it "should scope by perspective" do
+      LedgerItem.perspective(@recipient).count.should eql 1
+      LedgerItem.perspective(@sender).count.should eql 0
+    end
+  end
+
   describe "query scoping" do
     before(:each) do
       Factory(:ledger_item,
               :transacted_on => Date.new(2009, 1, 1))
     end
-    
+
     it "should scope transaction date on or after a date" do
       from = 'since Jan 15 2009'
       LedgerItem.scope_by(from).size.should == 1
     end
-    
+
     it "should scope transaction date on or before a date" do
       to = 'until Jan 15 2009'
       LedgerItem.scope_by(to).size.should == 1
     end
-    
+
     it "should not scope transaction date if date does not parse" do
       to = 'on Jan 15, 2009'
       LedgerItem.scope_by(to).size.should == 2

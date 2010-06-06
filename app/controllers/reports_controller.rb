81835ce7a92fc4cd2ad2660a6bc6e145bb31bef9
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-06-05 22:25:03 -0400

    Slimmed down controller, things are still ugly though

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,15 +2,6 @@ class ReportsController < ApplicationController
   before_filter :require_user
 
   def show
-    @self_select_options = Contact.self.collect{ |c| [c.name, c.id.to_s] }
-
-    if params[:base_currency].nil?
-      params[:contact] = @self_select_options.first.last
-      params[:base_currency] = 'USD'
-      params[:from_date] = Date.ordinal(Date.today.year - 1, 1)
-      params[:to_date] = Date.ordinal(Date.today.year - 1, 365)
-    end
-
-    @report = Report.new(params[:id].titleize)
+    @report = Report.new(params)
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,21 +1,40 @@
 class Report
   NAMES = ["Income Statement", "Balance Sheet"]
 
-  attr_reader :name
+  attr_reader :name, :perspective, :to_date, :from_date, :base_currency
 
-  def initialize(name)
-    if NAMES.include?(name)
-      @name = name
+  def initialize(params)
+    if NAMES.include?(params[:id].titleize)
+      @name = params[:id].titleize
     else
       raise "Unknown report name"
     end
+
+    if params[:base_currency].nil?
+      @perspective = self_select_options.first.last
+      @base_currency = 'USD'
+      @from_date = Date.ordinal(Date.today.year - 1, 1)
+      @to_date = Date.ordinal(Date.today.year - 1, 365)
+    else
+      %w{perspective to_date from_date base_currency}.each do |var_name|
+        instance_variable_set("@#{var_name}".to_sym, params[var_name.to_sym])
+      end
+    end
+  end
+
+  def available?
+    roots.any? { |a| a.grand_total_for?(perspective, from_date, to_date) }
   end
 
   def roots
     if name == 'Income Statement'
-      RevenueOrExpense.roots
+      RevenueOrExpense.roots + CurrencyConversion.roots
     elsif name == 'Balance Sheet'
       AssetOrLiability.roots
     end
+  end
+
+  def self_select_options 
+    @self_select_options ||= Contact.self.collect{ |c| [c.name, c.id.to_s] }
   end
 end

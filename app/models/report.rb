533a7e087511704c96450dc1ed30f0941ae56bf6
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-06-08 11:09:12 -0400

    Hackety intervention to sort root accounts so reports look more conventional. Accumulating technical debt as we speak!

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,24 +4,9 @@ class Report
   attr_reader :name, :perspective, :to_date, :from_date, :base_currency
 
   def initialize(params)
-    if NAMES.include?(params[:id].titleize)
-      @name = params[:id].titleize
-    else
-      raise "Unknown report name"
-    end
-
-    if params[:base_currency].nil?
-      @perspective = self_select_options.first.last
-      @base_currency = 'USD'
-      @from_date = Date.ordinal(Date.today.year - 1, 1)
-      @to_date = Date.ordinal(Date.today.year - 1, 365)
-    else
-      %w{perspective to_date from_date base_currency}.each do |var_name|
-        instance_variable_set("@#{var_name}".to_sym, params[var_name.to_sym])
-      end
-    end
-
-    @from_date = "1990-01-01" if name == "Balance Sheet"
+    @params = params
+    set_name
+    set_defaults
   end
 
   def available?
@@ -30,13 +15,47 @@ class Report
 
   def roots
     if name == 'Income Statement'
-      RevenueOrExpense.roots + CurrencyConversion.roots
+      sort_root_accounts(RevenueOrExpense.roots + CurrencyConversion.roots, true)
     elsif name == 'Balance Sheet'
-      AssetOrLiability.roots
+      sort_root_accounts(AssetOrLiability.roots)
     end
   end
 
   def self_select_options 
     @self_select_options ||= Contact.self.collect{ |c| [c.name, c.id.to_s] }
+  end
+
+  private
+
+  def set_name
+    if NAMES.include?(@params[:id].titleize)
+      @name = @params[:id].titleize
+    else
+      raise "Unknown report name"
+    end
+  end
+
+  def set_defaults
+    if @params[:base_currency].nil?
+      @perspective = self_select_options.first.last
+      @base_currency = 'USD'
+      @from_date = Date.ordinal(Date.today.year - 1, 1)
+      @to_date = Date.ordinal(Date.today.year - 1, 365)
+    else
+      %w{perspective to_date from_date base_currency}.each do |var_name|
+        instance_variable_set("@#{var_name}".to_sym, @params[var_name.to_sym])
+      end
+    end
+
+    # A balance sheet covers all ledger items from the beginning of (the) history
+    # of the company to the date requested
+    @from_date = "1990-01-01" if name == "Balance Sheet"
+  end
+
+  def sort_root_accounts(collection, reverse=false)
+    sign = reverse ? -1 : 1
+    collection.sort { |x, y|
+      sign * y.grand_total_for_in_base_currency(perspective, from_date, to_date, base_currency) <=> 
+      sign * x.grand_total_for_in_base_currency(perspective, from_date, to_date, base_currency) }
   end
 end

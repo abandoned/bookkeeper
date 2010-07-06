fe832d1296e3083f438b182b809219fba37bcec1
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-07-06 12:39:45 +0100

    Validate associated match. This will break some other stuff.

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -10,35 +10,28 @@ describe Match do
                           :sender => @recipient,
                           :recipient => @sender,
                           :total_amount => 20.0,
-                          :account => @debit_account,
-                          :transacted_on => "2009-01-01")
+                          :account => @debit_account)
     @credit_ledger_item = Factory(:ledger_item,
                            :sender => @sender,
                            :recipient => @recipient,
                            :total_amount => -20.0,
-                           :account => @credit_account,
-                           :transacted_on => "2009-01-01")
+                           :account => @credit_account)
     @match = Match.new
     @match.ledger_items << @debit_ledger_item
     @match.ledger_items << @credit_ledger_item
   end
 
-  it "does reconcile if sum of totals of matched ledger items is 0" do
+  it "should reconcile if sum of totals of matched ledger items is 0" do
     @match.reconciled?.should be_true
   end
   
-  it "does not validate if not reconciled" do
+  it "should not validate if not reconciled" do
     @debit_ledger_item.update_attribute :total_amount, 10.0
     lambda {@match.save!}.should raise_error(ActiveRecord::RecordInvalid)
   end
   
-  it "does not validate if matched ledger items have different currency" do
+  it "should not validate if matched ledger items have different currency" do
     @debit_ledger_item.update_attribute :currency, 'JPY'
-    lambda {@match.save!}.should raise_error(ActiveRecord::RecordInvalid)
-  end
-
-  it "does not validate if matched items do not have same date" do
-    @debit_ledger_item.update_attribute :transacted_on,"2010-01-01"
     lambda {@match.save!}.should raise_error(ActiveRecord::RecordInvalid)
   end
 end

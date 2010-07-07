0d805821225e04c50dda4e04de650653b6cafffe
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-07-07 17:34:46 +0100

    Fucking accounting

--- app/models/fx_appreciation.rb
+++ app/models/fx_appreciation.rb
@@ -22,13 +22,9 @@ class FxAppreciation
 
     def total_for_in_base_currency(perspective, from_date, to_date, base_currency, base_currency_date)
       roots = RevenueOrExpense.roots.all
-      sum_current = roots.sum do |a|
+      roots.sum do |a|
         a.grand_total_for_in_base_currency(perspective, "1990-01-01", Date.parse(from_date.to_s) - 1, base_currency, base_currency_date)
       end
-      sum_orig = roots.sum do |a|
-        a.grand_total_for_in_base_currency(perspective, "1990-01-01", Date.parse(from_date.to_s) - 1, base_currency, (Date.parse(from_date) - 1))
-      end
-      sum_current - sum_orig
     end
   end
 end
